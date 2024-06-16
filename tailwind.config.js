10c691c6ee8991242ce78fd82ca92594be9f367d
tempura327 <wanglt242@gmail.com>
2024-06-16 21:39:42 +0800

    chore: set tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,31 @@
 /** @type {import('tailwindcss').Config} */
+
+import { yellow, slate, red, orange } from 'tailwindcss/colors';
+
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: '28px',
+      },
+    },
+    fontFamily: {
+      primary: ['Roboto', 'Nunito Sans'],
+    },
+    extend: {
+      colors: {
+        primary: '#08A6BB',
+        secondary: yellow[400],
+        'neutral-light': '#CBCBCB',
+        'neutral-middle': '#AEAEAE',
+        'neutral-main': '#525151',
+        'neutral-dark': '#313131',
+        info: slate[500],
+        error: red[500],
+        warning: orange[500],
+      },
+    },
   },
-  plugins: [],
 };
